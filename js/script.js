0392ce9ea5be634ecafa4c76986c5095e92f9fea
marco-scocca <marcoscocca1991@gmail.com>
2024-07-09 07:09:20 +0200

    verifica che il contenuto sia uguale a quello del parametro string

--- js/script.js
+++ js/script.js
@@ -1,4 +1,10 @@
 function checkPalindroma(string) {
     let check_palindroma;
     check_palindroma = string.split('').reverse().join('');
+    if (check_palindroma.toLowerCase() === string.toLowerCase()) {
+        console.log('la stringa è palindroma');
+    }
+    else {
+        console.log('la stringa non è palindroma');
+    }
 }
